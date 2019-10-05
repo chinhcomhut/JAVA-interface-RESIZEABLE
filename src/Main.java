7bd8c54cd064a5947655de03f463fd15f6343c1f
- <chinhcomhut@gmail.com>
2019-10-05 22:32:21 +0700

    Update Resize viết thêm ngoại lệ THROW NEW

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(">>>>>>Pre Recize<<<<<<");
         Resizeable[] resizeables = new Resizeable[3];
         resizeables[0] = new Circle(3.6);
 //resizeables[1]=new Circle(1);
 //resizeables[2]=new Circle(3.5,"indigo",false);
-        resizeables[1]=new Rectangle(4,5);
-        resizeables[2]=new Square(5);
+        resizeables[1] = new Rectangle(4, 5);
+        resizeables[2] = new Square(5);
+        for (Resizeable resizeable : resizeables) {
+            System.out.println(resizeable);
+        }
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter percent (%): ");
         double percent = sc.nextDouble();
-        System.out.println(">>>>>>Pre Recize");
+        if(percent <=0||percent>100){
+            throw new IndexOutOfBoundsException("Không hợp lệ đăng nhập lại từ 1-100");
+        }else {
+            System.out.println(">>>>>> After-Resize <<<<<<<<");
+        }
+
 //        double percent = (Math.random()*100);
 //        System.out.println("Percent increased: "+percent);
 //Circle[] circles = new Circle[3];
@@ -25,14 +33,12 @@ public class Main {
 //}
 
 
-for(Resizeable resizeable:resizeables){
-    System.out.println(resizeable);
-}
-        System.out.println(">>>>>> After-Resize");
-for (Resizeable resizeable:resizeables){
-    resizeable.resize(percent);
-    System.out.println(resizeable);
-}
+
+
+        for (Resizeable resizeable : resizeables) {
+            resizeable.resize(percent);
+            System.out.println(resizeable);
+        }
 //Rectangle rectangle = new Rectangle();
 //        System.out.println(rectangle.getArea());
 
